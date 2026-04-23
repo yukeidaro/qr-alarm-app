7c54b7ebcd82c57de8be1837c96344ce81023c52
ukdayoharry <yukeiasano@gmail.com>
2026-04-23 21:02:00 +0900

    fix: move EAS pre-install hook to package.json scripts

--- eas-build-pre-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Fix permissions for files uploaded from Windows/OneDrive
-# Without this, prebuild fails with EACCES when creating .expo/web cache
-set -e
-chmod -R u+rwX .
-echo "Fixed permissions on project root"
